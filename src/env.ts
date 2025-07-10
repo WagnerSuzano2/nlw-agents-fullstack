03bc56bcc04a7bcd5a0751d8c1abb26465efec9f
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-09 22:18:09 -0300

    feat: Conexão inicial com PostgreSQL via Docker estabelecida

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,11 @@
-import {z} from 'zod';
+import { z } from 'zod';
 
 const envSchema = z.object({
-    //NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
-    PORT: z.coerce.number().default(3333),
-    // DATABASE_URL: z.string().url(),
-    // JWT_SECRET: z.string().min(32).max(64),
-    // API_KEY: z.string().optional(),
-})
+  //NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string().url().startsWith('postgresql://'),
+  // JWT_SECRET: z.string().min(32).max(64),
+  // API_KEY: z.string().optional(),
+});
 
 export const env = envSchema.parse(process.env);
-
