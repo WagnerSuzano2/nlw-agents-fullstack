c8b1ad27e6f0aa1af94fd27262f65ffb2eeaeb37
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-10 21:09:40 -0300

    feat: Implementa rota GET /rooms para listar quartos do banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import { fastifyCors } from '@fastify/cors';
-import { fastify } from 'fastify';
+import fastifyCors from '@fastify/cors'; // Corrigido!
+import fastify from 'fastify';
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
-//import { sql } from './db/connection.ts';
 import { env } from './env.ts';
+
+import { getRoomsRoute } from './http/routes/get-rooms.ts'; 
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -18,14 +19,13 @@ app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.get('/health', () => {
-  return { message: 'Hello World' };
+  // Rota /health
+  return { message: 'Hello World' }; // Agora retorna um objeto
 });
 
-app
-  .listen({
-    port: env.PORT,
-  })
-  .then(() => {
-   // console.log(`Port: ${env.PORT}`);
-    // console.log('HTTP server running');
-  });
+app.register(getRoomsRoute);
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`Port: ${env.PORT}`);
+  console.log('HTTP server running!');
+});
